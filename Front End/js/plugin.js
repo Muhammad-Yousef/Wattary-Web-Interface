8223447df4e206f5352ca6ce3b27a589be45de4e
Muhammad Swaaf <mohammedswaaf@gmail.com>
2018-03-13 16:25:46 +0200

    Made responding code

--- Front End/js/plugin.js
+++ Front End/js/plugin.js
@@ -7,38 +7,36 @@ $(document).ready(function(){
     // for nice scroll
     $("html").niceScroll();
 
+
+
+    // chat page
+
+
+
 });
 
   //section Animation colors
-
-
 $(document).ready(function(){
      $(".gear-option").click(function(){
          $(".color").show(1000);
      });
 });
-
 $(document).ready(function(){
      $(".color").click(function(){
          $(".color").hide(1000);
      });
 });
-
-
     // for change The color
 $(document).ready(function(){
     var ColorLi=$(".color ul li")
-
     ColorLi
     .eq(0).css("backgroundColor","#71ADB5").end()
     .eq(1).css("backgroundColor","#ffab00").end()
     .eq(2).css("backgroundColor","#964f8e").end()
     .eq(3).css("backgroundColor","#7FB414").end()
     .eq(4).css("backgroundColor","#12648c");
-
     ColorLi.click(function(){
         $("link[href*='theme']").attr("href",$(this).attr("data-value"));
-       //console.log($(this).attr("data-value"))
     });
 
     // for Scroll Up
@@ -59,6 +57,58 @@ $(document).ready(function(){
         $(scroll).click(function(){
             $("html,body").animate({scrollTop:0},2000)
         });
+
+
+
+
+        // Web chat page
+
+	// send
+
+    function newMessage() {
+        msg = $(".input-group input").val();
+        if($.trim(msg) === '') {
+            return false;
+        }
+        $('<li class="sent"><span>' + msg + '</span></li>').appendTo($('.msg .ch1'));
+        $('.input-group input').val(null);
+
+    };
+
+    $('.btn').click(function() {
+        newMessage();
+    });
+
+    $(window).on('keydown', function(e) {
+        if (e.which === 13) {
+            newMessage();
+            return false;
+        }
+    });
+
+    // Respond
+
+
+    function newMessage2() {
+        msg = $(".input-group2 input").val();
+        if($.trim(msg) === '') {
+            return false;
+        }
+        $('<li class="sent"><span>' + msg + '</span></li>').appendTo($('.msg .ch2'));
+        $('.input-group2 input').val(null);
+    };
+
+    $('.btn2').click(function() {
+        newMessage2();
+    });
+
+    $(window).on('keydown', function(e) {
+        if (e.which === 13) {
+            newMessage2();
+            return false;
+        }
+    });
+
 
 });
 
@@ -181,19 +231,18 @@ msg.onkeyup= function () {
 
 
 //Web Socket
-$(document).ready(function() {
-    var socket = io.connect('http://127.0.0.1:5000');
-    socket.on('connect', function() {
-        socket.send('User has connected!');
-    });
-    socket.on('message', function(msg) {
-        $("#messages").append('<li>'+msg+'</li>');
-        console.log('Received message');
-    });
-    $('#sendbutton').on('click', function() {
-        socket.send($('#myMessage').val());
-        $('#myMessage').val('');
-    });
-});
+// $(document).ready(function() {
+//     var socket = io.connect('http://127.0.0.1:5000');
+//     socket.on('connect', function() {
+//         socket.send('User has connected!');
+//     });
+//     socket.on('message', function(msg) {
+//         $("#messages").append('<li>'+msg+'</li>');
+//         console.log('Received message');
+//     });
+//     $('#sendbutton').on('click', function() {
+//         socket.send($('#myMessage').val());
+//         $('#myMessage').val('');
+//     });
+// });
 
-// web chat
